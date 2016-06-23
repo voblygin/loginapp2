ccbc793c981bc742a2ef031140083c28a89c9437
voblygin <cto@freee-wifi.ru>
2016-06-23 19:01:35 +0500

    registration complete

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+
+var User = require ('../models/user');
+
 //Get Homepage
 
 router.get('/register', function(req, res) {
@@ -18,7 +21,35 @@ router.post('/register', function(req, res){
     var password = req.body.password;
     var password2 = req.body.password2;
 
-    console.log(name);
+    //Validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Email is required').equals(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('register',{
+            errors:errors
+        });
+    } else {
+        var newUser = new User({
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        });
+
+        User.createUser(newUser, function(err,user){
+            if(err) throw err;
+            console.log(user);
+        });
+        req.flash('success_msg','You are registered and can now login');
+        res.redirect('/users/login');
+    }
 });
 
 module.exports = router;
